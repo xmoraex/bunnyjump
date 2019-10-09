0525c65d3928140fc936751bdb525a26fc996937
xmoraex <xunbeeiei@gmail.com>
2019-10-09 18:03:03 +0700

    Add first version

--- Player/Player.gd
+++ Player/Player.gd
@@ -3,7 +3,7 @@ extends KinematicBody2D
 var motion = Vector2(0,100)
 export var Gravity = 10
 export var SPEED = 500
-export var Jump_power = 500
+export var Jump_power = 450
 var jump_count = 0
 
 func _physics_process(delta):
@@ -11,8 +11,28 @@ func _physics_process(delta):
 	apply_gravity()
 	Jump()
 	walk()
+	update_animation()
 	
 	move_and_slide(motion, Vector2( 0, -1 ))
+	
+func update_animation():
+	if motion.y < 0:
+		$AnimatedSprite.play("jump")
+	elif motion.x < 0:
+		$AnimatedSprite.play("walk")
+		$AnimatedSprite.flip_h = true
+	elif motion.x > 0:
+		$AnimatedSprite.play("walk")
+		$AnimatedSprite.flip_h = false
+	
+	else:
+		$AnimatedSprite.play("stand")
+func is_dead():
+	
+	if position.y > 1000:
+		return true
+		
+	return false
 	
 func apply_gravity():
 	if is_on_floor():
@@ -25,14 +45,19 @@ func Jump():
 		if Input.is_action_just_pressed("jump"):
 			motion.y = -Jump_power
 			jump_count = jump_count + 1
+			$AudioStreamPlayer.play()
 	if is_on_floor():
 		jump_count = 0
+		
 
 func walk():
 	if Input.is_action_pressed("left"): 
 		motion.x = -SPEED
 	elif Input.is_action_pressed("right"):
 		motion.x = +SPEED
+		$AnimatedSprite.play("walk")
 	else:
 		motion.x = 0
+
+
 	
